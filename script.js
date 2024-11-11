45692b1abc112f664f05e21e6f1f43440b0227ef
ChitteGnanaPrasannKumarReddy <116110463+ChitteGnanaPrasannKumarReddy@users.noreply.github.com>
2024-11-11 13:01:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+function findDomLevel() {
+    let element = document.getElementById("level");
+    let level = 1; 
+
+    
+    while (element.parentElement) {
+        element = element.parentElement;
+        level++;
+    }
+
+    
+    alert("The level of the element is: " + level);
+}
+
+
+findDomLevel();
